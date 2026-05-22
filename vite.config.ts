ea5035e98960252d5bb45d338bedd5970a1c5ddf
TaroYamada2026 <suddenlyopens@gmail.com>
2026-05-22 16:33:25 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/test35/", // ★これを追加（前後のスラッシュを忘れないでください）
+  base: "/test36_2/", // ★これを追加（前後のスラッシュを忘れないでください）
 });
